754c3103d67efd3ac2847c401f2cc65a17f4d9e7
MimmoC <cacace.d98@gmail.com>
2016-12-12 16:20:42 +0100

    Added movement and rotation

---
+++ movement.cpp
@@ -0,0 +1,7 @@
+void rotateToPoint(float target[3]){
+    float temp[3];
+    getMyPos();
+    mathVecSubtract(temp, target, myPos, 3);
+    mathVecNormalize(temp, 3);
+    api.setAttitudeTarget(temp);
+}
