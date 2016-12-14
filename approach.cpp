ecbd446f3d224304b1b94fc24e6f2a81d59ccdb2
MimmoC <cacace.d98@gmail.com>
2016-12-14 09:42:32 +0100

    Edited distance multiplicatort for ranking

--- approach.cpp
+++ approach.cpp
@@ -1,55 +1,86 @@
-void calcPoint(){
-    game.getItemZRState(itemState, targetNumber);
-    if(itemState[3] <= 0.1 && itemState[4] <= 0.1 && itemState[5] <= 0.1){
-        game.getItemLoc(actualTarget, targetNumber);
-        for(int i=0; i<3; i++){
-            itemAtt[i] = itemState[i+6];
-            pointAtt[i] = -itemAtt[i];
-        }
-        mathVecNormalize(itemAtt, 3);
-        float length = mathVecMagnitude(itemAtt, 3);
-        for(int i=0; i<3; i++)
-            virtualTarget[i] = itemAtt[i] * distFromTarget/length*0.99 + actualTarget[i];
-        calculated = true;
+void worthyPack() {
+    for(int i = 0; i < 4; i++) {
+        getRank(i);
+    }
+    getWorthyPackInfo();
+    setDist();
+}
+
+/*cumulative function to learn which pack is the worthiest*/
+
+int itemStat(int num) {
+    if(game.itemInZone(num))
+        return 3;
+        
+    if(packInTheirZone(num) && game.hasItem(num) == 0)
+        return 4;
+        
+    return(game.hasItem(num));
+}
+
+/* we check if the pack is in our or their zone to calculate the real rating of the item*/
+
+void getRank(int num) {
+    float target[3];
+    getMyPos();
+    game.getItemLoc(target, num);
+    ranking[num] = 1/((dist(myPos, target))*(dist(myPos, target)));
+    switch(num){
+        case 0:   //large
+        case 1:
+            ranking[num]*=4.5;
+            break;
+        case 2:   //medium
+        case 3:
+            ranking[num]*=2.5;
+            break;
+    }
+    switch(itemStat(num)){
+        case 0:
+            ranking[num]*=1;
+            break;
+        case 4:
+            ranking[num]*=1.75;
+            break;
+        default:
+            ranking[num]*=-100;
+            break;
     }
 }
 
-/*this function calculates the docking point based on the position, attitude and type of item*/
+/* the rating is based on distance and points/second that the item gives*/
 
-void approachPack(){
-    if(!compareVector(myPos, virtualTarget, 0.005)){
-        float dis_to_point_near = dist(myPos, virtualTarget);
-        float dis_to_center = dist(myPos, actualTarget);
-        if ((dis_to_point_near>dis_to_center) && (dis_to_point_near<=0.4)){
-            goAround();
+void getWorthyPackInfo(){
+    targetNumber = 0;
+    float max = ranking[0];
+    for(int i = 0; i < 4; i++){
+        if(ranking[i] > max){
+            targetNumber = i;
+            max = ranking[i];
         }
-            else{
-            api.setPositionTarget(virtualTarget);
-        }
+    }
+    game.getItemLoc(actualTarget, targetNumber);
+}
+
+/*calculates the max valued pack */
+
+void setDist(){
+    switch(targetNumber){
+        case 0:
+        case 1:
+            //distFromTarget = .182;
+            distFromTarget = .162;
+            distMin = 0.151;
+            distMax = 0.173;
+            break;
+        case 2:
+        case 3:
+            //distFromTarget = .1725;
+            distFromTarget = .143;
+            distMin = 0.138;
+            distMax = 0.160;
+            break;
     }
 }
 
-
-/*we go to the docking point*/
-
-void goAround(){
-    float dist[3];
-    float max=-1;
-    int max_num=-1;
-    for(int i = 0; i<3; ++i){
-        dist[i] = fabsf(myState[i]-itemState[i]);
-        if (dist[i]>max){
-            max = dist[i];
-            max_num = i;
-        }
-    }
-    
-    float force_point[3];
-    for(int i =0; i<3;++i)
-        force_point[i] = itemAtt[i];
-    force_point[max_num]*=-1;
-    counter++;
-    if (counter % 2 == 0) api.setForces(force_point);
-}
-
-/*this function let us fly around an item to avoid collision*/
+/*set docking distances based on pack. (probably useless with the new functions we're using) */
