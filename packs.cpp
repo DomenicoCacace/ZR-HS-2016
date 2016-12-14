4b547aa2625ca3a3fae2176e29daf005bed6e229
Studente22 <Studente22>
2016-12-14 09:03:47 +0100

    Edited packs page

--- packs.cpp
+++ packs.cpp
@@ -1,33 +1,30 @@
-//calculates the worthiest pack to go for
-
 void worthyPack() {
-    for(int i = 0; i < 4; i++) {    //we ignore small packs, not worth at all
+    for(int i = 0; i < 4; i++) {
         getRank(i);
     }
     getWorthyPackInfo();
     setDist();
 }
 
-
-//details about position of the pack
+/*cumulative function to learn which pack is the worthiest*/
 
 int itemStat(int num) {
-    if(game.itemInZone(num))  //in our zone
+    if(game.itemInZone(num))
         return 3;
-
-    if(packInTheirZone(num) && game.hasItem(num) == 0)  //in their zone
+        
+    if(packInTheirZone(num) && game.hasItem(num) == 0)
         return 4;
-
-    return(game.hasItem(num));  //otherwise,
+        
+    return(game.hasItem(num));
 }
 
+/* we check if the pack is in our or their zone to calculate the real rating of the item*/
 
-//get rating for each pack based on their status
 void getRank(int num) {
     float target[3];
     getMyPos();
     game.getItemLoc(target, num);
-    ranking[num] = 1/(dist(myPos, target)); //the closer it is, the worthier it gets
+    ranking[num] = 1/(dist(myPos, target));
     switch(num){
         case 0:   //large
         case 1:
@@ -40,19 +37,19 @@ void getRank(int num) {
     }
     switch(itemStat(num)){
         case 0:
-            ranking[num]*=1;  //no special case
+            ranking[num]*=1;
             break;
         case 4:
-            ranking[num]*=1.75; //in their zone
+            ranking[num]*=1.75;
             break;
         default:
-            ranking[num]*=-100; //don't even consider that pack
+            ranking[num]*=-100;
             break;
     }
 }
 
+/* the rating is based on distance and points/second that the item gives*/
 
-//processes info about packs, determining the worthiest to go for
 void getWorthyPackInfo(){
     targetNumber = 0;
     float max = ranking[0];
@@ -65,21 +62,25 @@ void getWorthyPackInfo(){
     game.getItemLoc(actualTarget, targetNumber);
 }
 
+/*calculates the max valued pack */
 
-//set distances for docks based on our target
 void setDist(){
     switch(targetNumber){
-        case 0:   //large
+        case 0:
         case 1:
-            distFromTarget = .182;   //medium distance
+            //distFromTarget = .182;
+            distFromTarget = .162;
             distMin = 0.151;
-            distMax = 0.173;  
+            distMax = 0.173;
             break;
-        case 2: //medium
+        case 2:
         case 3:
-            distFromTarget = .1725;
+            //distFromTarget = .1725;
+            distFromTarget = .143;
             distMin = 0.138;
             distMax = 0.160;
             break;
     }
 }
+
+/*set docking distances based on pack. (probably useless with the new functions we're using) */
