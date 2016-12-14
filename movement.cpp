1502d167c34e2a5a3f23ef715044b4f49452403f
MimmoC <cacace.d98@gmail.com>
2016-12-14 09:40:52 +0100

    Removed setZoneInfo

--- movement.cpp
+++ movement.cpp
@@ -7,14 +7,3 @@ void rotateToPoint(float target[3]){
 }
 
 /*rotates to a point*/
-
-
-void setZonePoint(){
-    game.getItemLoc(actualTarget, targetNumber);
-    ourZonePos[0] = actualTarget[0];
-    ourZonePos[1] = actualTarget[1];
-    if(actualTarget[2]>0.1)
-        ourZonePos[2] = actualTarget[2] - 0.1;
-    else
-        ourZonePos[2] = actualTarget[2] + 0.1;
-}
