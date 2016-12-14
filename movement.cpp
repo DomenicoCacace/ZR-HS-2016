105701e7e785080e6fcd7d08561b2489e6205ea7
Studente22 <Studente22>
2016-12-14 09:00:40 +0100

    Edited movement page

--- movement.cpp
+++ movement.cpp
@@ -5,3 +5,16 @@ void rotateToPoint(float target[3]){
     mathVecNormalize(temp, 3);
     api.setAttitudeTarget(temp);
 }
+
+/*rotates to a point*/
+
+
+void setZonePoint(){
+    game.getItemLoc(actualTarget, targetNumber);
+    ourZonePos[0] = actualTarget[0];
+    ourZonePos[1] = actualTarget[1];
+    if(actualTarget[2]>0.1)
+        ourZonePos[2] = actualTarget[2] - 0.1;
+    else
+        ourZonePos[2] = actualTarget[2] + 0.1;
+}
