8c5fbe9f9b27efc1e25764e3c9512ba38a127d18
MimmoC <cacace.d98@gmail.com>
2016-12-15 20:15:07 +0100

    Vadim resurrected

--- movement.cpp
+++ movement.cpp
@@ -7,3 +7,60 @@ void rotateToPoint(float target[3]){
 }
 
 /*rotates to a point*/
+
+void moveTo(float* target){
+    float between[3];
+    for(int i =0; i<3; ++i){
+        between[i] = target[i] - myState[i];
+    }
+    
+    float curDis = dist(myState, target);
+    
+    if (curDis >= 0.1){ // distance on what it really shouldnt apply forces
+        if (counter % 4 == 0) api.setForces(between); //frequency of applying forces. frequency is more when number is less 
+        counter++;
+    }
+    api.setPositionTarget(target);
+}
+
+
+void goAround(int targetNumber){
+    game.getItemZRState(itemState, targetNumber);
+    float itemAtt[3], dist[3], b[3], c[3], forcePoint[3];
+    float max=-1;
+    int max_num=-1;
+    for(int i =0; i<3; ++i)
+        itemAtt[i] = itemState[6+i];
+    for(int i = 0; i<3; ++i){
+        dist[i] = fabsf(myState[i]-itemState[i]);
+        if (dist[i]>max){
+            max = dist[i];
+            max_num = i;
+        }
+    }
+    for(int i =0; i<3;++i)
+        forcePoint[i] = itemAtt[i];
+    forcePoint[max_num]*=-1;
+    mathVecAdd(c, forcePoint, itemAtt, 3);
+    mathVecNormalize(c, 3);
+    for(int i = 0; i < 3; ++ i)
+        c[i]/=12;
+    mathVecAdd(b, virtualTarget, c, 3);
+    moveTo(b);
+}
+
+void approachPack(){
+    float x[3];
+    float a, b, c;
+    for(int i = 0; i < 3; ++i)
+        x[i] = itemAtt[i] + actualTarget[i];
+    if(!compareVector(myPos, virtualTarget, 0.005)){
+        a = dist(myPos, virtualTarget);
+        b = dist(x, virtualTarget);
+        c = dist(myPos, x);
+        if(a*a + b*b < c*c)
+            goAround(targetNumber);
+        else
+            moveTo(virtualTarget);
+    }
+}
--- position.cpp
+++ position.cpp
@@ -1,5 +1,4 @@
 char ourColor(){
-    getMyPos();
     if(myPos[1] > 0)
         return 'B';
     else
@@ -11,35 +10,21 @@ char ourColor(){
 void zoneInfo(){
     float zoneData[4];
     game.getZone(zoneData);
-    assign(ourZone, zoneData[0], zoneData[1], zoneData[2]);
-    assign(theirZone, zoneData[0]*(-1), zoneData[1]*(-1), zoneData[2]*(-1));
+    for(int i = 0; i < 3; i++, ourZone[i] = zoneData[i], theirZone[i] = -zoneData[i]);
 }
 
 /*we get the location of our and their zone*/
 
-bool packInZone(){
-    float temp[3];
-    game.getItemLoc(temp, targetNumber);
-    if(compareVector(temp, ourZone, 0.05))
-        return true;
-    else 
-        return false; 
-}
-
-/*we check if a pack is in our zone or not*/
-
-bool packInTheirZone(int id){
-    float temp[3];
-    game.getItemLoc(temp, id);
-    if(compareVector(temp, theirZone, 0.08))
-        return true;
-    else
-        return false;
-}
 
 bool packIsMoving(int id){
     game.getItemZRState(itemState, id);
     return(itemState[3] != 0.00 || itemState[4] != 0.00 || itemState[5] != 0.00);
+}
+
+bool packInTheirZone(int id){
+    float temp[3];
+    game.getItemLoc(temp, id);
+    return(compareVector(temp, theirZone, 0.08));
 }
 
 /*we check if a pack is in their zone or not*/
@@ -51,7 +36,30 @@ void getMyPos() {
 
 /*we get our position*/
 
-void getTheirPos(){
-    api.getMyZRState(myState);
-    for(int i = 0; i < 3; i++, theirPos[i] = myState[i]);
+
+bool packInZone(){
+    float temp[3];
+    game.getItemLoc(temp, targetNumber);
+    if(compareVector(temp, ourZone, 0.055))
+        return true;
+    else 
+        return false; 
 }
+
+void calcPoint(){
+    game.getItemZRState(itemState, targetNumber);
+    if(itemState[3] <= 0.1 && itemState[4] <= 0.1 && itemState[5] <= 0.1){
+        game.getItemLoc(actualTarget, targetNumber);
+        for(int i=0; i<3; i++){
+            itemAtt[i] = itemState[i+6];
+            pointAtt[i] = -itemAtt[i];
+        }
+        mathVecNormalize(itemAtt, 3);
+        float length = mathVecMagnitude(itemAtt, 3);
+        for(int i=0; i<3; i++)
+            virtualTarget[i] = itemAtt[i] * distFromTarget/length*0.99 + actualTarget[i];
+        calculated = true;
+    }
+}
+
+/*this function calculates the docking point based on the position, attitude and type of item*/
